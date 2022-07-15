aed8e9a33331a25e5dd46adc3edd7ec80c8aa919
Jasper <jasper.delanghe@gmail.com>
2022-07-15 15:51:59 +0200

    Added GTM and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), partytown()]
 });
