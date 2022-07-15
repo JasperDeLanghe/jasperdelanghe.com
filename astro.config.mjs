28859b3b92c41e41f41e645cd36da194e9a6dfb8
Jasper <jasper.delanghe@gmail.com>
2022-07-15 15:57:07 +0200

    Added sitemap integration, which does not work yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react(), tailwind(), partytown()]
+  site: "https://jasperdelanghe.com",
+  integrations: [react(), tailwind(), partytown(), sitemap()],
 });
